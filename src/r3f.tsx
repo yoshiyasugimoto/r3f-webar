b5c51b436ace27cfdb9c9138cde3dba230be816c
杉本義弥 <y.sugimoto@churadata.okinawa>
2023-02-28 18:46:30 +0900

    fix

--- src/r3f.tsx
+++ src/r3f.tsx
@@ -25,7 +25,7 @@ const ThreeFiber = () => {
     setWebCamTexture()
   }, [])
 
-  const gltf = useLoader(GLTFLoader, 'yakiniku.glb')
+  // const gltf = useLoader(GLTFLoader, 'yakiniku.glb')
 
   return (
     <>
@@ -33,12 +33,12 @@ const ThreeFiber = () => {
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <pointLight position={[-10, -10, -10]} />
       <Controls isControl={true} />
-      <primitive
+      {/* <primitive
         object={gltf.scene}
         scale={[10, 10, 10]}
         position={[0, -2, 0]}
         rotation={[Math.PI / 8, 0, 0]}
-      />
+      /> */}
     </>
   )
 }
